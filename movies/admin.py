9a42cf23f2dcc8eb9393ffc9d882c41c8b9ea4f3
Diya2004-source <diyagajjar1220@gmail.com>
2026-02-05 20:32:00 +0530

    Delete movies directory

--- movies/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'release_year', 'is_premium')
-    list_filter = ('genre', 'is_premium')
-    search_fields = ('title',)
