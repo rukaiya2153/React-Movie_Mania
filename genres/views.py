a9f6f4fae478a6338e957bf11c04fbba8343673f
Diya2004-source <diyagajjar1220@gmail.com>
2026-02-05 20:31:31 +0530

    Delete genres directory

--- genres/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Genre
-from .serializers import GenreSerializer
-from rest_framework.permissions import IsAdminUser
-
-class GenreViewSet(ModelViewSet):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-    permission_classes = [IsAdminUser]
